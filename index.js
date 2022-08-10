f0ae05b6ecd00c7b23a7c05ce7081e87874da7a3
Diwakar241 <62773948+Diwakar241@users.noreply.github.com>
2022-08-10 11:42:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -43,6 +43,14 @@ for (let i = 0; i < num; i += 1) {
 
 // asynchronous function to perform "Selection Sort"
 async function selectionSort(delay = 300) {
+
+	// For selecting section having id "ele"
+	    var barval=document.getElementById("ele")
+	    // For dynamically Updating the selected element
+	      barval.innerHTML=
+	      "selection Sort";
+
+
 let bars = document.querySelectorAll(".bar");
 // Assign 0 to min_idx
 var min_idx = 0;
@@ -107,6 +115,13 @@ for (var i = 0; i < bars.length; i += 1) {
 	// Provide lightgreen color to the ith bar
 	bars[i].style.backgroundColor = " rgb(49, 226, 13)";
 }
+// To pause the execution of code for 200 milliseconds
+await new Promise((resolve) =>
+	setTimeout(() => {
+	resolve();
+}, 200)
+);
+barval.innerHTML="Sorting Visualizer";
 
 // To enable the button "Generate New Array" after final(sorted)
 document.getElementById("Button1").disabled = false;
@@ -132,6 +147,13 @@ document.getElementById("Button4").style.backgroundColor = "#6f459e";
 
 // asynchronous function to perform "Bubble Sort"
 async function BubbleSort(delay = 300) {
+
+	// For selecting section having id "ele"
+			var barval=document.getElementById("ele")
+			// For dynamically Updating the selected element
+				barval.innerHTML=
+				"Bubble Sort";
+
 let bars = document.querySelectorAll(".bar");
 for(var i=0;i<bars.length-1;i++){
   for(var j=0;j<bars.length-1-i;j++){
@@ -181,6 +203,13 @@ for(var i=0;i<bars.length-1;i++){
 }
 	// Provide lightgreen color to the 1st bar
 bars[0].style.backgroundColor = " rgb(49, 226, 13)";
+// To pause the execution of code for 200 milliseconds
+await new Promise((resolve) =>
+	setTimeout(() => {
+	resolve();
+}, 200)
+);
+barval.innerHTML="Sorting Visualizer";
 
 // To enable the button "Generate New Array" after final(sorted)
 document.getElementById("Button1").disabled = false;
@@ -199,6 +228,115 @@ document.getElementById("Button3").style.backgroundColor = "#6f459e";
 document.getElementById("Button4").disabled = false;
 document.getElementById("Button4").style.backgroundColor = "#6f459e";
 }
+
+
+
+
+
+// asynchronous function to perform "Insertion Sort"
+async function InsertionSort(delay = 300) {
+	// For selecting section having id "ele"
+	    var barval=document.getElementById("ele")
+	    // For dynamically Updating the selected element
+	      barval.innerHTML=
+	      "	Insertion Sort";
+
+
+
+	let bars = document.querySelectorAll(".bar");
+
+	  // Provide lightgreen colour to 0th bar
+	  bars[0].style.backgroundColor = " rgb(49, 226, 13)";
+	  for (var i = 1; i < bars.length; i += 1) {
+
+	    // Assign i-1 to j
+	    var j = i - 1;
+
+	    // To store the integer value of ith bar to key
+	    var key =
+	    parseInt(bars[i].childNodes[0].innerHTML);
+
+	    // To store the ith bar height to height
+	    var height = bars[i].style.height;
+
+	    //Provide darkblue color to the ith bar
+	    bars[i].style.backgroundColor = "darkblue";
+
+	    // To pause the execution of code for 600 milliseconds
+	    await new Promise((resolve) =>
+	    setTimeout(() => {
+	      resolve();
+	    }, 600)
+	  );
+
+	    // For placing selected element at its correct position
+	    while (j >= 0 && parseInt(bars[j].childNodes[0].innerHTML) > key) {
+
+	      // Provide darkblue color to the jth bar
+	      bars[j].style.backgroundColor = "darkblue";
+
+	      // For placing jth element over (j+1)th element
+	      bars[j + 1].style.height = bars[j].style.height;
+	      bars[j + 1].childNodes[0].innerText =
+	      bars[j].childNodes[0].innerText;
+
+	      // Assign j-1 to j
+	      j = j - 1;
+
+	      // To pause the execution of code for 600 milliseconds
+	      await new Promise((resolve) =>
+	        setTimeout(() => {
+	          resolve();
+	        }, 600)
+	      );
+
+	      // Provide lightgreen color to the sorted part
+	      for(var k=i;k>=0;k--){
+	        bars[k].style.backgroundColor = " rgb(49, 226, 13)";
+	      }
+	    }
+
+	    // Placing the selected element to its correct position
+	    bars[j + 1].style.height = height;
+	    bars[j + 1].childNodes[0].innerHTML = key;
+
+	    // To pause the execution of code for 600 milliseconds
+	    await new Promise((resolve) =>
+	      setTimeout(() => {
+	        resolve();
+	      }, 600)
+	    );
+
+	    // Provide light green color to the ith bar
+	    bars[i].style.backgroundColor = " rgb(49, 226, 13)";
+	  }
+		// To pause the execution of code for 200 milliseconds
+		await new Promise((resolve) =>
+			setTimeout(() => {
+			resolve();
+		}, 200)
+		);
+barval.innerHTML="Sorting Visualizer";
+// To enable the button "Generate New Array" after final(sorted)
+document.getElementById("Button1").disabled = false;
+document.getElementById("Button1").style.backgroundColor = "#6f459e";
+
+// To enable the button "Bubble Sort" after final(sorted)
+document.getElementById("Button2").disabled = false;
+document.getElementById("Button2").style.backgroundColor = "#6f459e";
+
+// To enable the button "Selection Sort" after final(sorted)
+document.getElementById("Button3").disabled = false;
+document.getElementById("Button3").style.backgroundColor = "#6f459e";
+
+
+// To enable the button "Insertion Sort" after final(sorted)
+document.getElementById("Button4").disabled = false;
+document.getElementById("Button4").style.backgroundColor = "#6f459e";
+}
+
+
+
 
 
 
